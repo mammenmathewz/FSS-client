4d8d62ee2a78979d4f630c3b26ddef4398628a50
Mammen Mathew <mammen999@gmail.com>
2025-02-05 21:37:39 +0530

    Add Tailwind CSS configuration and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <div>
-      <h1>Welcome to Full Stack Solutions Updated</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Full Stack Solutions Updated</h1>
       </div>
     </>
   )
